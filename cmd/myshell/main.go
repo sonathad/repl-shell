c808ca2a06dad6dc731cdfef07b7ba375428085b
sonathad <sonathad@proton.me>
2024-06-13 20:59:31 +0300

    check for exit 0

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,10 +12,12 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		cmd = strings.TrimSpace(cmd)
-		if cmd == "" {
-			break
+		switch cmd {
+		case "":
+			return
+		case "exit 0":
+			os.Exit(0)
 		}
 		fmt.Printf("%s: command not found\n", cmd)
 	}
-
 }
