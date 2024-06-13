3ee2a29126e4f140d6f4610834fce279f9e46213
sonathad <sonathad@proton.me>
2024-06-13 19:29:00 +0300

    print command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	// write to standard output
 	fmt.Fprint(os.Stdout, "$ ")
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd = strings.TrimSpace(cmd)
+	fmt.Printf("%s: command not found\n", cmd)
 }
