13d60625ae3e10bbfeb2d8cb64463436debe1b1c
sonathad <sonathad@proton.me>
2024-06-13 19:35:40 +0300

    accept arbitrary amount of input

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,10 +8,14 @@ import (
 )
 
 func main() {
-	// write to standard output
-	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
-	cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	cmd = strings.TrimSpace(cmd)
-	fmt.Printf("%s: command not found\n", cmd)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+		if cmd == "" {
+			break
+		}
+		fmt.Printf("%s: command not found\n", cmd)
+	}
+
 }
