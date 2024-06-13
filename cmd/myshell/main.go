558b7e7a8243d83ee039c352388b189d367e35f3
sonathad <sonathad@proton.me>
2024-06-13 21:36:21 +0300

    refactor

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,35 +8,38 @@ import (
 )
 
 func main() {
-	builtin := map[string]bool{
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+		checkCmd(cmd)
+	}
+}
+
+func checkCmd(cmd string) {
+	builtins := map[string]bool{
 		"echo": true,
 		"exit": true,
 		"type": true,
 	}
 
-	for {
-		fmt.Fprint(os.Stdout, "$ ")
-		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		cmd = strings.TrimSpace(cmd)
+	line := strings.Split(cmd, " ")
+	suf := strings.Join(line[1:], " ")
 
-		line := strings.Split(cmd, " ")
-		suf := strings.Join(line[1:], " ")
-
-		switch pre := line[0]; pre {
-		case "exit":
-			os.Exit(0)
-		case "":
-			return
-		case "echo":
-			fmt.Println(strings.TrimSpace(suf))
-		case "type":
-			if builtin[suf] {
-				fmt.Printf("%s is a shell builtin\n", suf)
-			} else {
-				fmt.Printf("%s: not found\n", suf)
-			}
-		default:
-			fmt.Printf("%s: command not found\n", cmd)
+	switch pre := line[0]; pre {
+	case "exit":
+		os.Exit(0)
+	case "":
+		return
+	case "echo":
+		fmt.Println(strings.TrimSpace(suf))
+	case "type":
+		if builtins[suf] {
+			fmt.Printf("%s is a shell builtin\n", suf)
+		} else {
+			fmt.Printf("%s: not found\n", suf)
 		}
+	default:
+		fmt.Printf("%s: command not found\n", cmd)
 	}
 }
