ddbfd925a0d15aa987f59472ad2d79f4e689e81b
sonathad <sonathad@proton.me>
2024-06-13 19:18:32 +0300

    add basic code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"bufio"
-	// Uncomment this block to pass the first stage
-	// "fmt"
+	"fmt"
 	"os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
-
+	// write to standard output
+	fmt.Fprint(os.Stdout, "$ ")
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
